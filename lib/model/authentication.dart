28da788acf00d977280083d82d59ad41381e1d05
Made Indra <made.indra@protonmail.com>
2020-12-27 18:22:23 +0700

    add getter to authentication class

--- lib/model/authentication.dart
+++ lib/model/authentication.dart
@@ -3,15 +3,38 @@ class Authentication {
   final String email;
   final String authToken;
   final String refreshToken;
+  final String ticket;
 
-  Authentication({this.id, this.email, this.authToken, this.refreshToken});
+  int get userId {
+    return id;
+  }
+
+  String get userEmail {
+    return email;
+  }
+
+  String get userAuthToken {
+    return authToken;
+  }
+
+  String get userRefreshToken {
+    return refreshToken;
+  }
+
+  String get userTicket {
+    return ticket;
+  }
+
+  Authentication(
+      {this.id, this.email, this.authToken, this.refreshToken, this.ticket});
 
   factory Authentication.fromJson(Map<String, dynamic> json) {
     return Authentication(
       id: json['data']['id'],
       email: json['data']['email'],
-      authToken: json['data']['authToken'],
+      authToken: json['data']['token'],
       refreshToken: json['data']['refreshToken'],
+      ticket: json['data']['ticket'],
     );
   }
 }
