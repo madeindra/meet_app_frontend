52f25d05848175ce78a50ac493431e748dd1b6b8
Made Indra <made.indra@protonmail.com>
2020-12-27 20:22:06 +0700

    chat function works now

--- lib/util/websocket.dart
+++ lib/util/websocket.dart
@@ -1,7 +1,37 @@
+import 'dart:developer';
+
+import 'package:web_socket_channel/io.dart';
 import 'package:web_socket_channel/web_socket_channel.dart';
 
+import './auth.dart';
+
 class Websocket {
-  void subscribe(WebSocketChannel channel) {
-    channel.sink.add('{"action": "subscribe", "topic": "test"}');
+  final Auth auth = new Auth();
+
+  String ticket;
+  WebSocketChannel channel;
+
+  getChannel() {
+    return channel;
+  }
+
+  void init() {
+    log("Socket ticket: ${auth.getSocketTicket()}");
+
+    this.ticket = auth.getSocketTicket();
+    this.channel = IOWebSocketChannel.connect(
+        'ws://10.0.2.2:8080/chat?ticket=${this.ticket}');
+
+    this.channel.sink.add('{"action": "subscribe"}');
+  }
+
+  void close() {
+    this.channel.sink.add('{"action": "unsubscribe"}');
+    this.channel.sink.close();
+  }
+
+  void send(int targetId, String content) {
+    this.channel.sink.add(
+        '{"action": "publish", "data": {"targetId": $targetId, "content": "$content"}}');
   }
 }
