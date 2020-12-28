6c58d8b31940e69b067c9a2a8d9be1bd1690bf78
Made Indra <made.indra@protonmail.com>
2020-12-28 20:53:09 +0700

    add fromJson user

--- lib/model/user.dart
+++ lib/model/user.dart
@@ -4,4 +4,12 @@ class User {
   final String email;
 
   User({this.id, this.name, this.email});
+
+  factory User.fromJson(Map<String, dynamic> json) {
+    return User(
+      id: json['id'],
+      name: json['name'],
+      email: json['email'],
+    );
+  }
 }
