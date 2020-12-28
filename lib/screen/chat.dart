409724e9cc2200faf916baef1fbd486c196b1ae5
Made Indra <made.indra@protonmail.com>
2020-12-28 11:26:58 +0700

    fix chat screen

--- lib/screen/chat.dart
+++ lib/screen/chat.dart
@@ -1,8 +1,16 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 
+import 'package:http/http.dart' as http;
 import 'package:web_socket_channel/web_socket_channel.dart';
 
+import '../widget/chat.dart';
+
 import '../util/websocket.dart';
+import '../util/auth.dart';
 
 class ChatPage extends StatefulWidget {
   @override
@@ -11,7 +19,38 @@ class ChatPage extends StatefulWidget {
 
 class _ChatPageState extends State<ChatPage> {
   final Websocket websocket = new Websocket();
-  final TextEditingController controller = TextEditingController();
+  final Auth auth = new Auth();
+  var futureChat;
+
+  Future<dynamic> getList() async {
+    log('Get chat list');
+
+    final http.Response response = await http.get(
+        'http://10.0.2.2:8080/api/v1/chat/details/',
+        headers: <String, String>{
+          HttpHeaders.authorizationHeader: 'Bearer ${auth.getAuthToken()}',
+          HttpHeaders.contentTypeHeader: 'application/json; charset=UTF-8',
+        });
+
+    log(response.statusCode.toString());
+    log(response.body.toString());
+
+    if (response.statusCode == 200) {
+      final res = jsonDecode(response.body);
+
+      return res["data"];
+    }
+
+    log('Failed login');
+    auth.clear();
+    return null;
+  }
+
+  @override
+  void initState() {
+    super.initState();
+    futureChat = getList();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -22,31 +61,20 @@ class _ChatPageState extends State<ChatPage> {
       body: Center(
         child: Column(
           children: <Widget>[
-            StreamBuilder(
-              stream: channel.stream,
-              builder: (context, snapshot) {
-                return snapshot.hasData
-                    ? Text(
-                        snapshot.data.toString(),
-                      )
-                    : Text(
-                        '',
-                      );
-              },
-            ),
-            TextField(
-              controller: controller,
-              decoration: InputDecoration(labelText: "Enter your message here"),
-            )
+            FutureBuilder(
+                future: futureChat,
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  if (snapshot.hasData) {
+                    return ChatWidget(snapshot.data);
+                  } else if (snapshot.hasError) {
+                    return Text("${snapshot.error}");
+                  }
+
+                  return CircularProgressIndicator();
+                })
           ],
         ),
       ),
-      floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.send),
-          onPressed: () {
-            websocket.send(3, controller.text);
-            controller.clear();
-          }),
     );
   }
 }
