cd2a30445164253e06e5ffbb97af8e7a4f86a403
Made Indra <made.indra@protonmail.com>
2020-12-28 11:27:27 +0700

    wip chatroom screen

---
+++ lib/screen/chatroom.dart
@@ -0,0 +1,54 @@
+import 'dart:convert';
+
+import 'package:flutter/material.dart';
+
+import 'package:web_socket_channel/web_socket_channel.dart';
+
+import '../util/websocket.dart';
+
+class ChatPage extends StatefulWidget {
+  @override
+  _ChatPageState createState() => _ChatPageState();
+}
+
+class _ChatPageState extends State<ChatPage> {
+  final Websocket websocket = new Websocket();
+  final TextEditingController controller = TextEditingController();
+
+  @override
+  Widget build(BuildContext context) {
+    websocket.init();
+    WebSocketChannel channel = websocket.getChannel();
+
+    return new Scaffold(
+      body: Center(
+        child: Column(
+          children: <Widget>[
+            StreamBuilder(
+              stream: channel.stream,
+              builder: (context, snapshot) {
+                return snapshot.hasData
+                    ? Text(
+                        jsonDecode(snapshot.data.toString())["data"]["content"],
+                      )
+                    : Text(
+                        '',
+                      );
+              },
+            ),
+            TextField(
+              controller: controller,
+              decoration: InputDecoration(labelText: "Enter your message here"),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.send),
+          onPressed: () {
+            websocket.send(3, controller.text);
+            controller.clear();
+          }),
+    );
+  }
+}
