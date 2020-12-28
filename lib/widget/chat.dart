f8d9196d0f18d0e54dd05b3fdb7c5a632b06b122
Made Indra <made.indra@protonmail.com>
2020-12-28 13:09:34 +0700

    modify chat widget

--- lib/widget/chat.dart
+++ lib/widget/chat.dart
@@ -1,16 +1,28 @@
 import 'package:flutter/material.dart';
 
+import '../util/auth.dart';
+
 class ChatWidget extends StatelessWidget {
-  final dynamic data;
+  final List<dynamic> data;
+  final int userId = Auth().getUserId();
 
   ChatWidget(this.data);
+
+  String getFriendId(int senderId, int targetId) {
+    return this.userId == senderId ? targetId.toString() : senderId.toString();
+  }
 
   @override
   Widget build(BuildContext context) {
     List<Widget> list = new List<Widget>();
 
-    data.forEach((item) => {list.add(Text(item["content"]))});
+    data.forEach((item) => {
+          list.add(Column(children: <Widget>[
+            Text(getFriendId(item["senderId"], item["targetId"])),
+            Text(item["content"])
+          ]))
+        });
 
-    return Row(children: list);
+    return Column(children: list);
   }
 }
